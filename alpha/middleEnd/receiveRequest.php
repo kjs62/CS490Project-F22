df242eb8b70a4863d15438185f3d3c45ad60293a
kjs62 <kjs62@njit.edu>
2022-09-27 01:22:53 -0400

    updated post field formatting

--- alpha/middleEnd/receiveRequest.php
+++ alpha/middleEnd/receiveRequest.php
@@ -1,10 +1,14 @@
 <?php
 
+$ucid = $_POST['ucid'];
+$pass = $_POST['password'];
+$requestType = $_POST['type'];
+
 $ch = curl_init();
 
 curl_setopt($ch, CURLOPT_URL,"https://afsaccess4.njit.edu/~sgs6");
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
+curl_setopt($ch, CURLOPT_POSTFIELDS, "ucid=$ucid&password=$pass&type=$requestType");
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
@@ -12,6 +16,6 @@ $server_output = curl_exec($ch);
 
 curl_close ($ch);
 
-print_r($server_output);
+echo $server_output;
 
 ?>
