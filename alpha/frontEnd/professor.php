da556d3f8561ac819b31bebcbc6abef7f78759c5
iab32 <90282180+iab32@users.noreply.github.com>
2022-09-28 11:03:11 -0400

    Add files via upload

---
+++ alpha/frontEnd/professor.php
@@ -0,0 +1,2 @@
+<h1>Teacher Landing Page</h1>
+<head><link rel="stylesheet" href="alpha.css"></head>
---
+++ alpha/frontEnd/student.php
@@ -0,0 +1,2 @@
+<h1>Student Landing Page</h1>
+<head><link rel="stylesheet" href="alpha.css"></head>
